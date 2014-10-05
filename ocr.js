0999500b27b123b01afc9fd69c9a8bfb62f4a9d9
fku <fku1127@gmail.com>
2014-10-05 23:51:15 +0800

    image noise filtering

--- ocr.js
+++ ocr.js
@@ -2,7 +2,7 @@ var tesseract = require('node-tesseract');
 
 
 // Recognize text of any language in any format
- tesseract.process('black-or-white.processed.png',function(err, text) {
+ tesseract.process('black-or-white.png',function(err, text) {
     if(err) {
        console.error('ERR: ' + err);
     } else {
--- pngjs.js
+++ pngjs.js
@@ -9,18 +9,44 @@ fs.createReadStream(filename)
   }))
   .on('parsed', function() {
 
-    for (var y = 0; y < this.height; y++) {
-        for (var x = 0; x < this.width; x++) {
-            var idx = (this.width * y + x) << 2;
-            if (this.data[idx] !== 0 ||
-                this.data[idx + 1] !== 0 ||
-                this.data[idx + 2] !== 0
-              ) {
-              this.data[idx] = 255;
-              this.data[idx+1] = 255;
-              this.data[idx+2] = 255;
-            }
+    for (var y = 1; y < this.height - 1; y ++) {
+      for (var x = 1; x < this.width - 1; x ++) {
+        var idx00 = (this.width * (y-1) + (x-1)) << 2;
+        var idx01 = (this.width * (y-1) +  x)    << 2;
+        var idx02 = (this.width * (y-1) + (x+1)) << 2;
+        var idx10 = (this.width *  y    + (x-1)) << 2;
+        var idx11 = (this.width *  y    +  x)    << 2;
+        var idx12 = (this.width *  y    + (x+1)) << 2;
+        var idx20 = (this.width * (y+1) + (x-1)) << 2;
+        var idx21 = (this.width * (y+1) +  x)    << 2;
+        var idx22 = (this.width * (y+1) + (x+1)) << 2;
+
+        if (((this.data[idx00] !== 255 && this.data[idx00+1] !== 255 && this.data[idx00+2] !== 255) &&
+             (this.data[idx22] !== 255 && this.data[idx22+1] !== 255 && this.data[idx22+2] !== 255)) ||
+            ((this.data[idx01] !== 255 && this.data[idx01+1] !== 255 && this.data[idx01+2] !== 255) && 
+             (this.data[idx21] !== 255 && this.data[idx21+1] !== 255 && this.data[idx21+2] !== 255)) ||
+            ((this.data[idx02] !== 255 && this.data[idx02+1] !== 255 && this.data[idx02+2] !== 255) &&
+             (this.data[idx20] !== 255 && this.data[idx20+1] !== 255 && this.data[idx20+2] !== 255)) ||
+            ((this.data[idx10] !== 255 && this.data[idx10+1] !== 255 && this.data[idx10+2] !== 255) &&
+             (this.data[idx12] !== 255 && this.data[idx12+1] !== 255 && this.data[idx12+2] !== 255))) {
+          this.data[idx11] = 0;
+          this.data[idx11+1] = 0;
+          this.data[idx11+2] = 0;
         }
+      }
+    }
+
+    for (var y = 0; y < this.height; y ++) {
+      for (var x = 0; x < this.width; x ++) {
+        var idx = (this.width * y + x) << 2;
+        if (this.data[idx] === 0 && this.data[idx+1] === 0 && this.data[idx+2] === 0) {
+        } else if (this.data[idx] === 255 && this.data[idx+1] === 255 && this.data[idx+2] === 255) {
+        } else {
+          this.data[idx] = 255;
+          this.data[idx+1] = 255;
+          this.data[idx+2] = 255;
+        }
+      }
     }
 
     this.pack().pipe(fs.createWriteStream('black-or-white.png'));
