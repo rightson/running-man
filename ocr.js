cd501186084c4819b57483c6c38ae1e82d606e76
fku <fku1127@gmail.com>
2014-10-05 12:27:02 +0800

    update oct

--- ocr.js
+++ ocr.js
@@ -2,7 +2,7 @@ var tesseract = require('node-tesseract');
 
 
 // Recognize text of any language in any format
- tesseract.process('Numbers.jpg',function(err, text) {
+ tesseract.process('black-or-white.processed.png',function(err, text) {
     if(err) {
        console.error('ERR: ' + err);
     } else {
